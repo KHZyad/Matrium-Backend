ce3920eca27f66db157a8e9a56091f2c898a8be5
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-01-11 18:19:51 +0100

    Create DeliveryProduct.py

---
+++ app/models/DeliveryProduct.py
@@ -0,0 +1,10 @@
+from app.models.db import db
+
+class DeliveryProduct(db.Model):
+    __tablename__ = 'delivery_product'
+
+    delivery_id = db.Column(db.BigInteger, db.ForeignKey('delivery.delivery_id'), primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('stock.product_id'), primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    product = db.relationship('Product', backref='delivery_products')
