36e344f7bc8760b51bcb50e73b84ee17a166d54f
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-01-11 01:03:22 +0100

    Update recipe.py
    add columns

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -6,7 +6,9 @@ class Recipe(db.Model):
 
     recipe_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.Text, nullable=False)
+    product_name = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.Enum('fixed', 'variable'), nullable=False)
+    total_price = db.Column(db.Numeric(10, 2), default=0.00)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))  
     created_by = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
 
