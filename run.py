4d8653dd336f8a3e44d7baef1126ee108e85c862
Zyad Kherraf <131729805+KHZyad@users.noreply.github.com>
2025-01-11 21:26:39 +0100

    Update run.py
    hope it will work

--- run.py
+++ run.py
@@ -1,29 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
-from app.models.db import db, init_db
+from app.models.db import db, init_db 
 from app.routes.recipe_routes import recipe_routes
 from app.routes import product_routes
-#hope it will work
+
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'mysql+pymysql://avnadmin:AVNS_64D7XhVDVS5mweyqAHs@mysql-615390b-matrium-24.h.aivencloud.com:21017/mrp'
-)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://avnadmin:AVNS_64D7XhVDVS5mweyqAHs@mysql-615390b-matrium-24.h.aivencloud.com:21017/mrp' )
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
 
 init_db(app)
 
-# Register blueprints with unique prefixes
-app.register_blueprint(recipe_routes, url_prefix='/recipes')
-app.register_blueprint(product_routes, url_prefix='/products')
-
-print("Blueprints registered successfully!")
+app.register_blueprint(recipe_routes, url_prefix='/')
+app.register_blueprint(product_routes, url_prefix='/')
 
 @app.route('/')
 def home():
     return "Welcome to the Flask App!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002, debug=True)
+    app.run(host='0.0.0.0', port=5002)
